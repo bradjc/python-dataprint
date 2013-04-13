bdb6dcd41913160520e5451f105edbdc233377f5
Brad Campbell <bradjc5@gmail.com>
2013-04-13 03:13:57 -0400

    added more tests for tabs

--- test/test.py
+++ test/test.py
@@ -11,3 +11,16 @@ fd = open("exist.file", "w")
 fd.write("add some content\n")
 dataprint.to_file(fd, a)
 fd.close()
+
+print dataprint.to_string(a, tabwidth=4)
+
+b = [['Name', 'Age', 'Color'],
+     ['Brad', 24, 'blue'],
+     ['Jeff', 20, 'yellow'],
+     ['ReallyLong Name', 100, 'aafdlksjfkld']]
+
+dataprint.to_newfile("notab.file", b)
+dataprint.to_newfile("tab.file", b, tabwidth=4)
+dataprint.to_newfile("tab1space.file", b, tabwidth=4, min_padding=1)
+dataprint.to_newfile("tab5.file", b, tabwidth=5, min_padding=1)
+dataprint.to_newfile("space_nogap.file", b, min_padding=0)
