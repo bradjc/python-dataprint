80ee4ecf8b1d49a81bd7afcbf964f9c262aecacd
Brad Campbell <bradjc5@gmail.com>
2013-04-13 03:20:15 -0400

    added more meta info

--- setup.py
+++ setup.py
@@ -2,5 +2,9 @@ from distutils.core import setup
 
 setup(name='DataPrint',
       version='0.1',
+      description='Well formatted tabular data printing.',
+      author='Brad Campbell',
+      author_email='bradjc@umich.edu',
+      url='https://github.com/bradjc/python-dataprint',
       packages=['dataprint'],
      )
